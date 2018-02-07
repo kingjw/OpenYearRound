b2eda56e18f5186250a09d056267395f635f11bc
kingjw <kingjw007@naver.com>
2018-02-07 14:57:24 +0000

    게시판

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,6 +6,9 @@ var act=document.getElementsByClassName('act_Container')[0];
 var intro=document.getElementsByClassName('introduce_Container')[0];
 var apply=document.getElementsByClassName('apply_Container')[0];
 var notice=document.getElementsByClassName('notice_Container')[0];
+var modal = document.getElementById('myModal');
+var btn = document.getElementById('writeButton');
+var span = document.getElementsByClassName('close')[0];
 button1.onclick=function(){
   $('html').css('overflow','visible');
   act.style.display="block";
@@ -37,4 +40,16 @@ button4.onclick=function(){
   apply.style.display="none";
   notice.style.display="block";
     $('html,body').animate({'scrollTop':$(notice).offset().top}, 500);
+}
+btn.onclick=function(){
+  modal.style.display = "block";
+}
+span.onclick=function(){
+  modal.style.display="none";
+}
+//모달 밖을 클릿했을떄 닫히는것
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
