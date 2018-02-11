d76fa05d2f1cf41d3b9729784c8e4f140ddde5d4
seongjin571 <gkstjdwls11@naver.com>
2018-02-11 07:34:16 +0000

    수민 디자인 UI 적용

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,35 @@
-var button1=document.getElementById('button1');
-var button2=document.getElementById('button2');
-var button3=document.getElementById('button3');
-var button4=document.getElementById('button4');
+var introButton=document.getElementById('introButton');
+var supportButton=document.getElementById('supportButton');
+var noticeButton=document.getElementById('noticeButton');
+var actButton=document.getElementById('actButtonM');
+
+var introMenu=document.getElementsByClassName('menu')[0];
+var actMenu=document.getElementsByClassName('menu')[1];
+var noticeMenu=document.getElementsByClassName('menu')[2];
+var applyMenu=document.getElementsByClassName('menu')[3];
+
 var act=document.getElementsByClassName('act_Container')[0];
 var intro=document.getElementsByClassName('introduce_Container')[0];
 var apply=document.getElementsByClassName('apply_Container')[0];
 var notice=document.getElementsByClassName('notice_Container')[0];
+
 var modal = document.getElementById('myModal');
 var btn = document.getElementById('writeButton');
 var span = document.getElementsByClassName('close')[0];
-button1.onclick=function(){
+var moveTop=document.getElementsByClassName('moveTop')[0];
+$(window).scroll(function() {
+ var scroll = $(window).scrollTop();
+if (scroll==0){
+  moveTop.style.display='none';
+}
+else{
+  moveTop.style.display='block';
+}
+});
+moveTop.onclick=function(){
+  $('html,body').animate({'scrollTop':0}, 500);
+}
+actButton.onclick=function(){
   $('html').css('overflow','visible');
   act.style.display="block";
   intro.style.display="none";
@@ -17,29 +37,62 @@ button1.onclick=function(){
   notice.style.display="none";
   $('html,body').animate({'scrollTop':$(act).offset().top}, 500);
 }
-button2.onclick=function(){
+introButton.onclick=function(){
   $('html').css('overflow','visible');
   act.style.display="none";
   intro.style.display="block";
   apply.style.display="none";
   notice.style.display="none";
-    $('html,body').animate({'scrollTop':$(intro).offset().top}, 500);
+  $('html,body').animate({'scrollTop':$(intro).offset().top}, 500);
 }
-button3.onclick=function(){
+supportButton.onclick=function(){
   $('html').css('overflow','visible');
   intro.style.display="none";
   act.style.display="none";
   apply.style.display="block";
   notice.style.display="none";
-    $('html,body').animate({'scrollTop':$(apply).offset().top}, 500);
+  $('html,body').animate({'scrollTop':$(apply).offset().top}, 500);
 }
-button4.onclick=function(){
+noticeButton.onclick=function(){
   $('html').css('overflow','visible');
   intro.style.display="none";
   act.style.display="none";
   apply.style.display="none";
   notice.style.display="block";
-    $('html,body').animate({'scrollTop':$(notice).offset().top}, 500);
+  $('html,body').animate({'scrollTop':$(notice).offset().top}, 500);
+}
+
+introMenu.onclick=function(){
+  $('html').css('overflow','visible');
+  act.style.display="none";
+  intro.style.display="block";
+  apply.style.display="none";
+  notice.style.display="none";
+  $('html,body').animate({'scrollTop':$(intro).offset().top}, 500);
+}
+actMenu.onclick=function(){
+  $('html').css('overflow','visible');
+  act.style.display="block";
+  intro.style.display="none";
+  apply.style.display="none";
+  notice.style.display="none";
+  $('html,body').animate({'scrollTop':$(act).offset().top}, 500);
+}
+noticeMenu.onclick=function(){
+  $('html').css('overflow','visible');
+  intro.style.display="none";
+  act.style.display="none";
+  apply.style.display="none";
+  notice.style.display="block";
+  $('html,body').animate({'scrollTop':$(notice).offset().top}, 500);
+}
+applyMenu.onclick=function(){
+  $('html').css('overflow','visible');
+  intro.style.display="none";
+  act.style.display="none";
+  apply.style.display="block";
+  notice.style.display="none";
+  $('html,body').animate({'scrollTop':$(apply).offset().top}, 500);
 }
 btn.onclick=function(){
   modal.style.display = "block";
@@ -49,7 +102,17 @@ span.onclick=function(){
 }
 //모달 밖을 클릿했을떄 닫히는것
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
+
+$("input").focus(function(){
+  $(this).animate({borderColor: '#3899EC'},'slow');
+  this._placeholder = this.placeholder;
+  $(this).removeAttr('placeholder');
+});
+$("input").focusout(function(){
+  $(this).animate({borderColor: '#EEEEEE'},'slow');
+  this.placeholder = this._placeholder;
+});
