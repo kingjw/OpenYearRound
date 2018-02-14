94bb49db95df7bc9bb27265d9713ec3092bf08b9
kingjw <kingjw007@naver.com>
2018-02-14 14:48:33 +0000

    routes

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,8 @@ var dbconfig = require('../database.js');
 var conn = mysql.createConnection(dbconfig);
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var sql='select * from postboard;';//지원자 정보를 불러오는 쿼리문 저장
-  conn.query(sql,function(error,results,fields){
-    if(error){//데이터베이스에서 불러올 때 오류 메세지 띄워줌
-      console.log(error);
-      console.log('get postboard information failed');
-    }
-    else{//화면 렌더링 할 때 보내는 값, 사이트 제목, 지원자 정보
-      res.render('index',{
-        title:'openyearround', // 사이트 제목
-        board : results // 지원자 정보
-      });//render
-    }
-  });//query
-  //res.render('applyadmin',{title:'apply admin page'});
+  res.render('index',{
+    title:'openyearround', // 사이트 제목
+  });//render
 });
 module.exports = router;
