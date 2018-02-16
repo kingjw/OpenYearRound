838cbc092b16af9cd23c95bdd218c73cf0d99f3b
Ubuntu <ubuntu@ip-172-31-6-247.us-west-2.compute.internal>
2018-02-16 08:14:56 +0000

    로고 모달 끝

--- public/javascripts/webService.js
+++ public/javascripts/webService.js
@@ -1,69 +1,35 @@
-var dawson=document.getElementsByClassName('logoClick')[0];
-var dawsonClose=document.getElementsByClassName('webServiceClose')[0];
-var dawsonContent=document.getElementById('dawson');
-dawson.onclick=function(){
-  dawsonContent.style.display='block';
-}
-dawsonClose.onclick=function(){
-  dawsonContent.style.display='none';
-}
 
-var bookChange=document.getElementsByClassName('logoClick')[1];
-var bookChangeClose=document.getElementsByClassName('webServiceClose')[1];
-var bookChangeContent=document.getElementById('bookChange');
-bookChange.onclick=function(){
-  bookChangeContent.style.display='block';
-}
-bookChangeClose.onclick=function(){
-  bookChangeContent.style.display='none';
-}
 
-var mynote=document.getElementsByClassName('logoClick')[2];
-var mynoteClose=document.getElementsByClassName('webServiceClose')[2];
-var mynoteContent=document.getElementById('mynote');
-mynote.onclick=function(){
-  mynoteContent.style.display='block';
-}
-mynoteClose.onclick=function(){
-  mynoteContent.style.display='none';
-}
+$(".logoClick").click(function(){
+  var index = $(".logoClick").index(this);
+    $(".webServiceModal:eq(" + index + ")").fadeIn(500);
+});
 
-var sejongPlate=document.getElementsByClassName('logoClick')[3];
-var sejongPlateClose=document.getElementsByClassName('webServiceClose')[3];
-var sejongPlateContent=document.getElementById('sejongPlate');
-sejongPlate.onclick=function(){
-  sejongPlateContent.style.display='block';
-}
-sejongPlateClose.onclick=function(){
-  sejongPlateContent.style.display='none';
-}
+$(".webServiceClose").click(function(){
+  var index = $(".webServiceClose").index(this);
+    $(".webServiceModal:eq(" + index + ")").fadeOut(500);
+});
 
-var tabStorage=document.getElementsByClassName('logoClick')[4];
-var tabStorageClose=document.getElementsByClassName('webServiceClose')[4];
-var tabStorageContent=document.getElementById('tabStorage');
-tabStorage.onclick=function(){
-  tabStorageContent.style.display='block';
-}
-tabStorageClose.onclick=function(){
-  tabStorageContent.style.display='none';
-}
 
-var shopIn=document.getElementsByClassName('logoClick')[5];
-var shopInClose=document.getElementsByClassName('webServiceClose')[5];
-var shopInContent=document.getElementById('shopIn');
-shopIn.onclick=function(){
-  shopInContent.style.display='block';
+$(".left").click(function(){
+  var index = $(".left").index(this);
+if (index==0) {
+  var before=6
 }
-shopInClose.onclick=function(){
-  shopInContent.style.display='none';
+else{
+  var before= index-1;
 }
-
-var keepPet=document.getElementsByClassName('logoClick')[6];
-var keepPetClose=document.getElementsByClassName('webServiceClose')[6];
-var keepPetContent=document.getElementById('keepPet');
-keepPet.onclick=function(){
-  keepPetContent.style.display='block';
+    $(".webServiceModal:eq(" + index + ")").css('display','none');
+    $(".webServiceModal:eq(" + before + ")").css('display','block')
+});
+$(".right").click(function(){
+  var index = $(".right").index(this);
+  if(index==6){
+    var next=0
+  }
+  else{
+  var next = index+1;
 }
-keepPetClose.onclick=function(){
-  keepPetContent.style.display='none';
-}
+    $(".webServiceModal:eq(" + index + ")").css('display','none');
+    $(".webServiceModal:eq(" + next + ")").css('display','block')
+});
