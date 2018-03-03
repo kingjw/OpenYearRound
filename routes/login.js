733c0d8e7e5e5f6dc85b56df96f967355855c36a
kingjw <kingjw007@naver.com>
2018-03-03 19:35:11 +0000

    login

--- routes/login.js
+++ routes/login.js
@@ -10,4 +10,29 @@ router.get('/', function(req, res, next) {
     title:'openyearround', // 사이트 제목
   });//render
 });
+
+router.post('/gologin',function(req,res,next){
+  var id = req.body.id;
+  var password = req.body.password;
+
+  var sql = "select * from oyr_user where user_name=?";
+  conn.query(sql,[id], function(error,results,fields){
+    if(error){
+      console.log(id);
+
+    } else {
+      var user = results[0];
+      if(!user){
+        console.log('id fail');
+        res.send({results:'error'});
+      } else if(password == user.password){
+        console.log('okokok');
+        res.send({results:'success'});
+      } else {
+        console.log('fail');
+        res.send({results:'error'});
+      }
+    }
+  });
+});
 module.exports = router;
