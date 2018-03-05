ea4237b56ebe817c10b98379a404ad7887221741
seongjin571 <gkstjdwls11@naver.com>
2018-03-05 16:04:50 +0000

    브랜치 테스트용

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var mysql = require('mysql');
 var dbconfig = require('../database.js');
 var conn = mysql.createConnection(dbconfig);
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   if(req.session.authId){
     res.redirect('/');
