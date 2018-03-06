9188fb34a0dc6e610763d17b4d11ac9a7bc2912f
kingjw <kingjw007@naver.com>
2018-03-06 08:27:28 +0000

    asdasdasd

--- routes/board.js
+++ routes/board.js
@@ -11,14 +11,17 @@ router.post('/goBoard',function(req,res,next){//올리기 버튼 클릭 시 ajax
   var date = req.body.date;
   var sql = 'insert into `postboard` (`title`,`text`,`date`) values (?,?,?);';
   //입력한 정보를 테이블에 저장하는 쿼리문
+
   conn.query(sql,[title,contents,date],function(error,results,fields){
     if(error){
       console.log(error);
       console.log('postboard insert failed');
     }//if
-    else{
+    else if(req.session.authId){
       console.log(date);
       res.send({result:'success'});//ajax 통신이 성공하면 다시 success 메세지를 보냅니다.
+    } else {
+      console.log('else error');
     }
   });//query
 });//router post
