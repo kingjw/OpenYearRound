117558456c038c3a8b878334ef9e2deca2aa04f7
seongjin571 <gkstjdwls11@naver.com>
2018-03-08 15:34:54 +0000

    전체적 수정

--- routes/check.js
+++ routes/check.js
@@ -8,11 +8,9 @@ var conn = mysql.createConnection(dbconfig);
 router.post('/goCheck', function(req, res, next) {
 
   if(req.session.author == 'boarder_Admin' || req.session.author == 'superior_Admin'){
-    console.log('boarder or superior admin');
     res.send({results:100});
   }
   else{
-    console.log('not admin');
     res.send({results:200});
   }
 });
