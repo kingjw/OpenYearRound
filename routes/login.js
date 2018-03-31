bd62af33266fdf6ac0d2eb67aefdd0d3b1b37763
Ubuntu <ubuntu@ip-172-31-21-14.ap-northeast-2.compute.internal>
2018-03-31 15:40:13 +0000

    abcdef

---
+++ routes/login.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+var dbconfig = require('../database.js');
+var conn = mysql.createConnection(dbconfig);
+
+router.get('/', function(req, res, next) {
+  if(req.session.authId){
+    res.redirect('/');
+  }
+  else{
+    res.render('login',{
+      title:'openyearround', // 사이트 제목
+    });//render
+  }
+});
+
+router.post('/gologin',function(req,res,next){
+  var id = req.body.id;
+  var password = req.body.password;
+
+  var sql = "select * from oyr_user where user_name=?";
+  conn.query(sql,[id], function(error,results,fields){
+    if(error){
+      console.log(id);
+
+    } else {
+      var user = results[0];
+      if(!user){
+        console.log('id fail');
+        res.send({result:'error'});
+      } else if(password == user.password){
+        req.session.authId = id;
+        req.session.author = user.authorize;
+        req.session.save(function() {
+          res.send({result:'success'});
+        });
+      } else {
+        res.send({result:'error'});
+      }
+    }
+  });
+});
+router.get('/logout',function(req,res){
+  delete req.session.authId;
+  delete req.session.author;
+  res.redirect('/');
+});
+
+module.exports = router;
